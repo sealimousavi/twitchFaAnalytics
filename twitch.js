9b6bb8967d2d1f8af49d40542e339e2dbea013c6
sealimousavi <sealimosavi.am@gmail.com>
2022-03-06 13:32:03 +0330

    dynamicly change added

--- twitch.js
+++ twitch.js
@@ -6,11 +6,13 @@
 
       async function getData() {
         // get streamers and views name list
+
         const response = await fetch(api_url);
         const twitchData = await response.json();
-        const streamers = [0];
-        const views = [0];
+        const streamers = [];
+        const views = [];
         const twitchfaData = twitchData.data;
+        
         for (let i = 0; i < twitchfaData.length; i++) {
           streamers.push(twitchfaData[i].displayName);
           views.push(twitchfaData[i].viewers);
@@ -19,10 +21,12 @@
         const ys = views;
         return { xs, ys };
       }
+
       // creating chart
       async function chartIt() {
         const data = await getData();
         const ctx = document.getElementById("myChart").getContext("2d");
+        
         const myChart = new Chart(ctx, {
           type: "line",
           data: {
@@ -39,20 +43,20 @@
             ],
           },
         });
-        //   async function adddata() {
-        //     const new_data = await getData();
 
-        // //     if (myChart.data.datasets[0].data != new_data.ys) {
-        //       console.log("namosavi");
-        //       console.log(myChart.data.datasets[0].data);
-        //       console.log(new_data.ys);
-        //     }
-        //     myChart.data.datasets[0].data = new_data.ys;
-        //     Chart.update;
-        //   }
-        //   adddata();
+        // function for updating dynamicly
+
+           async function adddata() {
+             const new_data = await getData();
+             myChart.data.datasets[0].data = new_data.ys;
+             myChart.data.labels  = new_data.xs; 
+             console.log(new_data.ys)
+             myChart.update()
+            }
+
+        setInterval(adddata, 5000);
+
       }
 
       chartIt();
 
-      //setInterval(chartIt, 1000);
